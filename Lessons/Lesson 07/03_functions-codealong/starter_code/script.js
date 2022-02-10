add63281dfec5e4d1137bc6407feae422bba7dc9
imanidasilva <imani.dasilva@gmail.com>
2022-02-09 21:38:20 -0500

    HW04 in progress

--- Lessons/Lesson 07/03_functions-codealong/starter_code/script.js
+++ Lessons/Lesson 07/03_functions-codealong/starter_code/script.js
@@ -5,16 +5,47 @@
 // Create a function called `squared` that takes one argument
 // The function will return the argument times itself (the square of the number)
 
+function squared(victim) { // victim only exsits in function 
+    if (typeof victim != "number") { //bonus check for typeof
+        return 0;
+    }
+    //multiply the param by itself
+    victim = victim * victim;
+
+    // send back value
+    return victim;
+}
 console.log(squared(2));
 console.log(squared(5));
 console.log(squared(-3));
+console.log(squared("Parrots"));
 
 // BONUS: Check if the argument is a number first! (Hint: conditionals - if statements)
+
 
 // 2. Get string length!
 
 // Create a function called `stringLength` that returns the length of a string (how many characters?)
 // So: stringLength("hello") would return 5
+
+function stringLength(x) {
+
+    return x.length;
+
+
+    // //create variable to hold string length
+
+    // let theLength = 0;
+
+    // //get the length of the string
+
+    // theLength = x.length; //changing variable
+
+    // //return x
+
+    // return theLength;
+
+}
 
 console.log(stringLength("testing"))
 console.log(stringLength("This is a longer string."))
@@ -24,12 +55,35 @@ console.log(stringLength("This is a longer string."))
 // Create a function called addNumbers
 // Takes two arguments and returns the sum of those arguments
 
-// console.log(addNumbers(4, 2));
-// console.log(addNumbers(6, 3));
+function addNumbers(x, y){
+    //validating input
+    if(typeof x != "number"){
+        x = parseFloat(x);
+        if(isNaN(x)){
+            return NaN;
+        }
+    }
+    if(typeof y != "number"){
+        x = parseFloat(y);
+        if(isNaN(y)){
+            return NaN;
+        }
+    }
+    return x + y;
+}
+
+console.log(addNumbers(4, 2));
+console.log(addNumbers(6, 3));
 
 // 4. Create a function called sayHello
 // Takes one argument
 // Returns a string that is the combination of "Hello, " + the argument passed into the function
 // So sayHello("Paul") would return "Hello, Paul";
 
-// console.log(sayHello("Paul"));
+function sayHello(theName){
+
+    let output = "Hello, " + theName;
+
+    return output;
+}
+console.log(sayHello("Paul"));
